10fc00641016013e35e7f69c8ad35a2034945eba
Rafael Menchaca <arafael.menchaca@gmail.com>
2025-09-22 20:35:52 -0500

    fix: bug en login, key missed

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://bfnkaqmhcsyxdxoqnahk.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // tu clave anon pública
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmbmthcW1oY3N5eGR4b3FuYWhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzMDI4MTcsImV4cCI6MjA2Mzg3ODgxN30.ADYrubfJuP9Oo60713UldzO0owCIgYfUKJ8WFnuBCpM'; // tu clave anon pública
 
 // Cliente global
 window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
